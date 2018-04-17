4c1f3e1bbdef95afdd15ce8016e46607f0d27268
Felipe Goulart <flpgoulart@gmail.com>
2018-04-17 17:16:06 +0000

    adding occupation area model

---
+++ app/models/occupation_area.rb
@@ -0,0 +1,2 @@
+class OccupationArea < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417111340) do
+ActiveRecord::Schema.define(version: 20180417171330) do
+
+  create_table "occupation_areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "target_audiences", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
